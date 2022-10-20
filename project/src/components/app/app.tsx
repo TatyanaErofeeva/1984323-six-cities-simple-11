3d593fb3daf6ac4a0b6c4c89334c261a0ca353b4
TatyanaErofeeva <pupchik@mail.ru>
2022-10-20 08:13:28 +0000

    Создание страниц, динамика (props)

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppCardsProps = {
+  cardsCount: number;
+}
+
+function App({cardsCount}: AppCardsProps): JSX.Element {
+  return (
+    <MainPage cardsCount={cardsCount}/>
+  );
 }
 
 export default App;
